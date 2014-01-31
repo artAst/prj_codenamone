1b3283f835cb286bd17d73c835056d0dea7b362f
Art Astillero <ArtAstillero@Art-Astillero.local>
2014-01-31 14:50:07 +0800

    changes on the HTTPWebUtils functionality

--- src/userclasses/utilities/HTTPWebUtils.java
+++ src/userclasses/utilities/HTTPWebUtils.java
@@ -1,24 +1,43 @@
 package userclasses.utilities;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import com.codename1.components.WebBrowser;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.NetworkManager;
 import com.codename1.javascript.JavascriptContext;
 import com.codename1.ui.BrowserComponent;
 import com.codename1.ui.Component;
 
 public class HTTPWebUtils {
+	static String response = "";
 	
 	public static JavascriptContext getJSContextByUrl(String url) {
-		/*boolean hasLoaded = false;
-		WebBrowser browser = new WebBrowser(){
+		ConnectionRequest req = new ConnectionRequest(){
+			int chr;
+			StringBuffer sb = new StringBuffer();
+			
 			@Override
-            public void onLoad(String url) {
-				
+			protected void readResponse(InputStream input) throws IOException {
+				//reading the answer                      
+				while ((chr = input.read()) != -1){
+					sb.append((char) chr);
+				}
+				response = sb.toString();                                           
+				response = response.trim();
 			}
 		};
-		browser.setURL(url);
-		Component c = browser.getInternal();*/
-		BrowserComponent b = new BrowserComponent();
-		b.setURL(url);
+		req.setPost(false);
+		req.setUrl(url);
+		NetworkManager.getInstance().addToQueueAndWait(req);
+		
+		System.out.println(response);
+		WebBrowser browser = new WebBrowser();
+		browser.setPage(response, "http://localhost/");
+		response = "";
+		Component c = browser.getInternal();
+		BrowserComponent b = (BrowserComponent) c;
 		JavascriptContext ctx = new JavascriptContext(b);
 		return ctx;
 	}
